d7e64d889412cd880b6c0e17d810651918d55c70
Xian Cao (Beyondsoft) <caoxuan2011@hotmail.com>
2019-10-25 14:25:27 +0800

    new file

--- demo_test.py
+++ demo_test.py
@@ -1,5 +1,4 @@
 from pypinyin import lazy_pinyin,Style
-Passage=input('输入中文:')
-Passage=str(Passage)
+Passage=str(input('输入中文:'))
 style=Style.TONE
 print(lazy_pinyin(Passage,style=style,errors='ignore'))
